91c07d3ce06fbbac4a202293e40db71c27e541be
Daniel Zeng <danielzeng24@gmail.com>
2021-03-31 19:48:24 -0400

    cleaned up some stuff

--- python/app.py
+++ python/app.py
@@ -97,6 +97,8 @@ dwg = svgwrite.Drawing('test.svg', profile='tiny')
 dwg.add(dwg.line((0, 0), (10, 0), stroke=svgwrite.rgb(10, 10, 16, '%')))
 dwg.add(dwg.text('Test', insert=(0, 0.2)))
 dwg.save()
+
+
 '''
 features = c.get_features(did, wid, eid)
 f = features.json()
